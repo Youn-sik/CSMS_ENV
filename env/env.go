d34f1caff0e089276d877220639445959528e21f
Youn-sik <yscho20@gmail.com>
2023-10-17 17:57:13 +0900

    환경부 연동 API 프레임 생성

--- env/env.go
+++ env/env.go
@@ -1,1 +1,52 @@
 package envc
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+var envHttpClient *http.Client
+var bid string
+var bkey string
+
+func init() {
+	envHttpClient = GetHttpClient()
+	bid = "XX"
+	bkey = "XXXXXXXXXXXXXXXX"
+}
+
+func GetHttpClient() *http.Client {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	return &http.Client{Transport: tr}
+}
+
+func EnvHttpRequest[TQ EnvHttpRequestInterface, TR EnvHttpResponseInterface](url string, req TQ) (TR, error) {
+	var res TR
+
+	reqBody, _ := json.Marshal(req)
+	reqBodyBuff := bytes.NewBuffer(reqBody)
+	request, err := http.NewRequest("POST", "http://10.101.160.34/r2"+url, reqBodyBuff)
+	if err != nil {
+		return res, err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := envHttpClient.Do(request)
+	if err != nil {
+		return res, err
+	}
+
+	body, _ := ioutil.ReadAll(response.Body)
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return res, err
+	}
+
+	return res, err
+}
---
+++ env/envFunc.go
@@ -0,0 +1,521 @@
+package envc
+
+import (
+	"log"
+
+	envType "CSMS_ENV/env/type"
+	"CSMS_ENV/logger"
+)
+
+// 2.1 충전소 조회 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '이전 요청 이후 충전소 변경 분 정보를 조회'합니다.
+// 요청이 없을 때, '최대 12시간 이내 변경 정보'만 조회합니다.
+func chargerInfoList() {
+	var req envType.ChargerInfoListReq
+	req.Rbid = bid
+	req.Bkey = bkey
+	req.Kind = "1"
+
+	res, err := EnvHttpRequest[envType.ChargerInfoListReq, envType.ChargerInfoListRes]("/charger/info/list", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 2.2 충전소 전체 조회 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '충전소 전체 정보'를 조회합니다. '페이지 조건을 통해 일정 건수로 조회'합니다.
+// 최초 요청 시 페이지 및 전체 충전소 개수에 따라 재요청이 필요합니다.
+func chargerInfoListAll() {
+	var req envType.ChargerInfoListAllReq
+	req.Rbid = bid
+	req.Bkey = bkey
+	req.Kind = "1"
+	req.Pageno = "1"
+	req.Rowcnt = "1000"
+
+	res, err := EnvHttpRequest[envType.ChargerInfoListAllReq, envType.ChargerInfoListAllRes]("/charger/info/listall", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 2.3 충전소 상태 조회 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '충전기 상태 정보'를 조회합니다. '마지막 요청 이후 변경된 상태 정보'만 조회합니다.
+// 30분 이상 상태가 갱신되지 않은 경우 상태 미확인(9)로 조회됩니다.
+// 이전 요청이 없을 때, '최대 10분 이내 변경 정보'만 조회합니다.
+func chargerStatusList() {
+	var req envType.ChargerStatusListReq
+	req.Rbid = bid
+	req.Bkey = bkey
+	req.Kind = "1"
+
+	res, err := EnvHttpRequest[envType.ChargerStatusListReq, envType.ChargerStatusListRes]("/charger/status/list", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 2.4 충전소 상태 전체 조회 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '충전기 상태 전체 정보'를 조회합니다. '페이지 조건을 통해 일정 건수로 조회'합니다.
+// 1분 이내 충전기 상태가 조회됩니다.
+// 최초 요청 시 페이지 및 전체 충전기 개수에 따라 재요청이 필요합니다.
+func chargerStatusListAll() {
+	var req envType.ChargerStatusListAllReq
+	req.Rbid = bid
+	req.Bkey = bkey
+	req.Kind = "1"
+	req.Pageno = "1"
+	req.Rowcnt = "1000"
+
+	res, err := EnvHttpRequest[envType.ChargerStatusListAllReq, envType.ChargerStatusListAllRes]("/charger/status/listall", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 3.1 충전기 관리 정보 조회 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '사업자가 관리하는 충전기 정보'를 조회합니다.
+// 응답 건수가 5,000 건 초과인 경우 조회가 불가능합니다.
+func chargerInfoMyList() {
+	var req envType.ChargerInfoMyListReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Kind = "1"
+	req.Pageno = "1"
+	req.Rowcnt = "1000"
+
+	res, err := EnvHttpRequest[envType.ChargerInfoMyListReq, envType.ChargerInfoMyListRes]("/charger/info/mylist", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 3.2 충전기 상태 갱신 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '사업자가 관리하는 충전기 상태를 갱신'합니다.
+// 운영기관(자사)의 충전기 상태가 변경되는 경우, 1분 이내에 갱신합니다.
+// 갱신 이후 30분이 지난 경우, '상태미확인' 상태로 서비스에 제공됩니다.
+func chargerStatusUpdate() {
+	cstat := []envType.ChargerStatus{}
+	var req envType.ChargerStatusUpdateReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Cstat = cstat
+
+	res, err := EnvHttpRequest[envType.ChargerStatusUpdateReq, envType.ChargerStatusUpdateRes]("/charger/status/update", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 4.1 회원카드 조회 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '공동사용 협약한 사용자간 회원카드'를 조회합니다.
+// 타 사업자가 보유한 회원카드의 등록 결과를 조회합니다.
+// kind = 1 은 '이전 요청 이후 회원카드 변경 분 정보'를 조회합니다.
+// 요청이 없으면, 24시간 이내 변경 정보만 조회합니다.
+func cardList() {
+	var req envType.CardListReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Kind = "1"
+
+	res, err := EnvHttpRequest[envType.CardListReq, envType.CardListRes]("/card/list", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 4.2 회원카드 전체 조회 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '회원카드 정체 정보'를 조회합니다. '페이지 조건을 통해 일정 건수로 조회'합니다.
+// 최초 요청 시 페이지 및 전체 충전소 개수에 따라 재요청이 필요합니다.
+func cardListAll() {
+	var req envType.CardListAllReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Kind = "1"
+	req.Pageno = "1"
+	req.Rowcnt = "1000"
+
+	res, err := EnvHttpRequest[envType.CardListAllReq, envType.CardListAllRes]("/card/listall", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 4.3 회원카드 갱신 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '회원카드 상태가 변경되면 갱신'합니다.
+// 정지 회원이 다른 사업자 충전기에서 인증이 될 수도 있습니다.
+// 회원카드 변경 정보를 10분 이내에 갱신하고, 갱신 누락을 대비하여 하루 한 번 전체 갱신을 권고합니다.
+// 한 번에 2000건 까지 갱신 가능합니다.
+func cardUpdate() {
+	cardNoStop := []envType.CardNoStop{}
+	var req envType.CardUpdateReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Card = cardNoStop
+
+	res, err := EnvHttpRequest[envType.CardUpdateReq, envType.CardUpdateRes]("/card/update", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 4.4 회원카드 통계 조회 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '회원카드 사용자의 상태(정지)별 건수'를 조회합니다.
+func cardStat() {
+	var req envType.CardStatReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Rbid = ""
+
+	res, err := EnvHttpRequest[envType.CardStatReq, envType.CardStatRes]("/card/stat", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 5.1 충전이력 조회 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '로밍에 등록 된 시간 기준'으로 조회합니다.
+// 마지막 요청 이후 충전이력 등록 정보를 조회합니다.
+// 이전 요청이 없으면, 24시간 이내 등록된 정보가지만 조회합니다.
+func tradeList() {
+	var req envType.TradeListReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Kind = "1"
+
+	res, err := EnvHttpRequest[envType.TradeListReq, envType.TradeListRes]("/trade/list", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 5.2 충전이력 전체 조회 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '서버세 등록 된 시간 기준'으로 조회합니다.
+// 건수가 많을 경우 페이지 단위로 조회 가능합니다.
+// 최초 요청 시 페이지 및 전체 충전소 개수에 따라 재요청이 필요합니다.
+func tradeListAll() {
+	var req envType.TradeListAllReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Kind = "1"
+	req.Start = "2023100100000000"
+	req.End = "2023101800000000"
+	req.Pageno = "1"
+	req.Rowcnt = "1000"
+
+	res, err := EnvHttpRequest[envType.TradeListAllReq, envType.TradeListAllRes]("/trade/listall", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 5.3 충전이력 등록 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '회원카드 인증을 통해 발생한 충전이력'을 등록합니다.
+// 한 번에 2000건 까지 등록 가능합니다.
+// 잘못 전송 된 데이터는 별도 관리자 화면에서 데이터를 수정하거나, 삭제 후 재전송 합니다.
+func tradeRegister() {
+	tradeRegisterInfo := []envType.TradeRegister{}
+	var req envType.TradeRegisterReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Trade = tradeRegisterInfo
+
+	res, err := EnvHttpRequest[envType.TradeRegisterReq, envType.TradeRegisterRes]("/trade/regi", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 5.4 충전이력 제외 조회 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '등록한 사업자만 조회' 가능합니다.
+// 등록한 충전이력 중 이상 데이터로 검출될 경우, 정산 제외 처리됩니다.
+// 제외된 데이터를 확인하여 잘못된 데이터인 경우, 관리사이트에서 데이터 삭제 후 다시 등록합니다.
+// 응답 건수가 5000 건 초과인 경우 조회가 불가능합니다.
+func tradeExList() {
+	var req envType.TradeExlistReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Kind = "1"
+
+	res, err := EnvHttpRequest[envType.TradeExlistReq, envType.TradeExlistRes]("/trade/exlist", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 5.5 충전이력 통계 조회 (자세한 내용은 환경부 연동 문서 붙임 1 참고)
+// 해당 요청은 '충전 이력과 조회 대상 충전 이력에 대한 데이터의 건수과 충전량, 충전 금액의 합산 금액'을 조회합니다.
+// 기관별, 결제 종류별, 충전량, 충전금액 합계가 조회됩니다.
+func tradeStat() {
+	var req envType.TradeStatReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Rbid = ""
+	req.Start = ""
+	req.End = ""
+
+	res, err := EnvHttpRequest[envType.TradeStatReq, envType.TradeStatRes]("/trade/stat", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 6.1 이용정보 조회 (자세한 내용은 환경부 문서 붙임 1 참고)
+// 해당 요청은 '자사 충전 이용 정보'를 조회합니다.
+// 페이지 조건을 통해 일정 건수로 조회합니다.
+// 조회 등록 실시 순으로 조회됩니다.
+func useList() {
+	var req envType.UseListReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Kind = "1"
+	req.Tbase = ""
+	req.Start = ""
+	req.End = ""
+	req.Bdate = ""
+	req.Pageno = "1"
+	req.Rowcnt = "1000"
+
+	res, err := EnvHttpRequest[envType.UseListReq, envType.UseListRes]("/use/list", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 6.2 이용정보 등록 (자세한 내용은 환경부 문서 붙임 1 참고)
+// 해당 요청은 '자사 충전 이용 정보'를 등록합니다.
+// 중복 등록 되지 않으며 (sid, cid, tsdt 가 일치하는 경우) 변경이 필요한 경우 삭제 후 재등록 합니다.
+func useRegister() {
+	useRegisterInfo := []envType.UseRegister{}
+	var req envType.UseRegisterReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Use = useRegisterInfo
+
+	res, err := EnvHttpRequest[envType.UseRegisterReq, envType.UseRegisterRes]("/use/regi", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 6.3 이용정보 삭제 (자세한 내용은 환경부 문서 붙임 1 참고)
+// 해당 요청은 '자사 충전 이용 정보'를 삭제합니다.
+// 충전소, 충전기 ID, 충전시작일시 및 종료일시가 일치하는 정보를 삭제합니다.
+func useDelete() {
+	useDeleteInfo := []envType.UseDelete{}
+	var req envType.UseDeleteReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Use = useDeleteInfo
+
+	res, err := EnvHttpRequest[envType.UseDeleteReq, envType.UseDeleteRes]("/use/del", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
+
+// 6.4 이용정보 통계 조회 (자세한 내용은 환경부 문서 붙임 1 참고)
+// 해당 요청은 '자사 충전기 이용 정보 건수와 충전량, 충전 금액 합산 금액'을 조회합니다.
+func useStat() {
+	var req envType.UseStatReq
+	req.Bid = bid
+	req.Bkey = bkey
+	req.Tbase = ""
+	req.Start = ""
+	req.End = ""
+
+	res, err := EnvHttpRequest[envType.UseStatReq, envType.UseStatRes]("/use/stat", req)
+	if err != nil {
+		// 실패 에러 처리
+		logger.PrintErrorLogLevel4(err)
+		return
+	}
+	if res.Result == "2" || res.Result == "1" {
+		// (부분) 실패 에러 처리
+		log.Println(res.Errcode)
+		log.Println(res.Errdtl)
+		return
+	}
+
+	// 성공 처리
+}
