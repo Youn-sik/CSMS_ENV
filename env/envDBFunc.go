971e64d4cca5b075b4a7d25039aacf80138d5fd3
Youn-sik <yscho20@gmail.com>
2023-10-26 09:08:22 +0900

    Transaction Daed lock 문제에 따른 delete/insert -> update 쿼리로 변경

--- env/envDBFunc.go
+++ env/envDBFunc.go
@@ -117,38 +117,38 @@ func updateChargePoint[TR envType.ChargerInfoInterface](cp TR) (bool, string) {
 
 	chargePoint := cp.GetChargerInfo()
 	for _, val := range chargePoint {
-		_, err = tx.Exec("delete from charge_point where bid = ? and cid = ?", val.Bid, val.Cid)
-		if err != nil {
-			logger.PrintErrorLogLevel4(err)
-			return false, "Failed To Delete Data"
-		}
-		_, err = tx.Exec("insert into charge_point (bid, sid, cid, zcode, station_area_name, addr, addrdtl, daddr, daddrdtl, kind, kinddtl, gps, usetime, "+
-			"free, freedtl, bname, bcall, type, reserv, member, pay, fee, cable, status, statdt, note, "+
-			"bmngid, limityn, limitdetail, delyn, deldetail, last_tsdt, last_tedt, now_tsdt, method, output) "+
-			"values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-			val.Bid, val.Sid, val.Cid, val.Zcode, val.Name, val.Addr, val.Addrdtl, val.Daddr, val.Daddrdtl, val.Kind, val.Kinddtl, val.Gps, val.Usetime,
-			val.Free, val.Freedtl, val.Bname, val.Bcall, val.Type, val.Reserv, val.Member, val.Pay, val.Fee, val.Cable, val.Status, val.Statdt, val.Note,
-			val.Bmngid, val.Limityn, val.Limitdetail, val.Delyn, val.Deldetail, val.LastTsdt, val.LastTedt, val.NowTsdt, val.Method, val.Output)
-		if err != nil {
-			logger.PrintErrorLogLevel4(err)
-			return false, "Failed To Insert Data"
-		}
-		// 또는 아래 코드
 		/*
-			_, err = tx.Exec("update charge_point set sid = ?, zcode = ?, station_area_name = ?, addr = ?, addrdtl = ?, "+
-				"daddr = ?, daddrdtl = ?, kind = ?, kinddtl = ?, gps = ?, usetime = ?, "+
-				"free = ?, freedtl = ?, bname = ?, bcall = ?, type = ?, reserv = ?, member = ?, pay = ?, fee = ?, cable = ?, status = ?, statdt = ?, note = ?, "+
-				"bmngid = ?, limityn = ?, limitdetail = ?, delyn = ?, deldetail = ?, last_tsdt = ?, last_tedt = ?, now_tsdt = ?, method = ?, output = ?) "+
-				"where bid = ? and cid = ?",
-				val.Sid, val.Zcode, val.Name, val.Addr, val.Addrdtl,
-				val.Daddr, val.Daddrdtl, val.Kind, val.Kinddtl, val.Gps, val.Usetime,
-				val.Free, val.Freedtl, val.Bname, val.Bcall, val.Type, val.Reserv, val.Member, val.Pay, val.Fee, val.Cable, val.Status, val.Statdt, val.Note,
-				val.Bmngid, val.Limityn, val.Limitdetail, val.Delyn, val.Deldetail, val.LastTsdt, val.LastTedt, val.NowTsdt, val.Method, val.Output, val.Bid, val.Cid)
+			_, err = tx.Exec("delete from charge_point where bid = ? and cid = ?", val.Bid, val.Cid)
 			if err != nil {
 				logger.PrintErrorLogLevel4(err)
-				return false, "Failed To Update Data"
+				return false, "Failed To Delete Data"
+			}
+			_, err = tx.Exec("insert into charge_point (bid, sid, cid, zcode, station_area_name, addr, addrdtl, daddr, daddrdtl, kind, kinddtl, gps, usetime, "+
+				"free, freedtl, bname, bcall, type, reserv, member, pay, fee, cable, status, statdt, note, "+
+				"bmngid, limityn, limitdetail, delyn, deldetail, last_tsdt, last_tedt, now_tsdt, method, output) "+
+				"values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+				val.Bid, val.Sid, val.Cid, val.Zcode, val.Name, val.Addr, val.Addrdtl, val.Daddr, val.Daddrdtl, val.Kind, val.Kinddtl, val.Gps, val.Usetime,
+				val.Free, val.Freedtl, val.Bname, val.Bcall, val.Type, val.Reserv, val.Member, val.Pay, val.Fee, val.Cable, val.Status, val.Statdt, val.Note,
+				val.Bmngid, val.Limityn, val.Limitdetail, val.Delyn, val.Deldetail, val.LastTsdt, val.LastTedt, val.NowTsdt, val.Method, val.Output)
+			if err != nil {
+				logger.PrintErrorLogLevel4(err)
+				return false, "Failed To Insert Data"
 			}
 		*/
+		// 또는 아래 코드
+		_, err = tx.Exec("update charge_point set sid = ?, zcode = ?, station_area_name = ?, addr = ?, addrdtl = ?, "+
+			"daddr = ?, daddrdtl = ?, kind = ?, kinddtl = ?, gps = ?, usetime = ?, "+
+			"free = ?, freedtl = ?, bname = ?, bcall = ?, type = ?, reserv = ?, member = ?, pay = ?, fee = ?, cable = ?, status = ?, statdt = ?, note = ?, "+
+			"bmngid = ?, limityn = ?, limitdetail = ?, delyn = ?, deldetail = ?, last_tsdt = ?, last_tedt = ?, now_tsdt = ?, method = ?, output = ?) "+
+			"where bid = ? and cid = ?",
+			val.Sid, val.Zcode, val.Name, val.Addr, val.Addrdtl,
+			val.Daddr, val.Daddrdtl, val.Kind, val.Kinddtl, val.Gps, val.Usetime,
+			val.Free, val.Freedtl, val.Bname, val.Bcall, val.Type, val.Reserv, val.Member, val.Pay, val.Fee, val.Cable, val.Status, val.Statdt, val.Note,
+			val.Bmngid, val.Limityn, val.Limitdetail, val.Delyn, val.Deldetail, val.LastTsdt, val.LastTedt, val.NowTsdt, val.Method, val.Output, val.Bid, val.Cid)
+		if err != nil {
+			logger.PrintErrorLogLevel4(err)
+			return false, "Failed To Update Data"
+		}
 	}
 
 	tx.Commit()
@@ -264,26 +264,26 @@ func updateEnvCard[TR envType.CardInfoInterface](ci TR) (bool, string) {
 
 	card := ci.GetEnvCardInfo()
 	for _, val := range card {
-		_, err = tx.Exec("delete from env_card where bid = ? and no = ?", val.Bid, val.No)
-		if err != nil {
-			logger.PrintErrorLogLevel4(err)
-			return false, "Failed To Delete Data"
-		}
-		_, err = tx.Exec("insert into env_card(bid, no, stop, regdate, upddate) values(?,?,?,?,?)",
-			val.Bid, val.No, val.Stop, val.Regdate, val.Upddate)
-		if err != nil {
-			logger.PrintErrorLogLevel4(err)
-			return false, "Failed To Insert Data"
-		}
-		// 또는 아래 코드
 		/*
-			_, err = tx.Exec("update env_card set stop = ?, regdate = ?, upddate = ? where bid = ? and no = ?",
-				val.Stop, val.Regdate, val.Upddate, val.Bid, val.No)
+			_, err = tx.Exec("delete from env_card where bid = ? and no = ?", val.Bid, val.No)
 			if err != nil {
 				logger.PrintErrorLogLevel4(err)
-				return false, "Failed To Update Data"
+				return false, "Failed To Delete Data"
+			}
+			_, err = tx.Exec("insert into env_card(bid, no, stop, regdate, upddate) values(?,?,?,?,?)",
+				val.Bid, val.No, val.Stop, val.Regdate, val.Upddate)
+			if err != nil {
+				logger.PrintErrorLogLevel4(err)
+				return false, "Failed To Insert Data"
 			}
 		*/
+		// 또는 아래 코드
+		_, err = tx.Exec("update env_card set stop = ?, regdate = ?, upddate = ? where bid = ? and no = ?",
+			val.Stop, val.Regdate, val.Upddate, val.Bid, val.No)
+		if err != nil {
+			logger.PrintErrorLogLevel4(err)
+			return false, "Failed To Update Data"
+		}
 	}
 
 	tx.Commit()
